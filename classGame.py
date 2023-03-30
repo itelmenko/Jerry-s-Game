652a6c5eea1edf38a40c55fe8486579f117e1db4
Igor Telmenko <igor-telmenko@yandex.ru>
2023-03-30 20:53:42 +0300

    Clap sound

--- classGame.py
+++ classGame.py
@@ -6,14 +6,14 @@ class Game():
     def __init__(self):
         self.font = pygame.font.SysFont('DOCKER THREE', 40)
 
-
         self.FPS = 30
         self.clock = pygame.time.Clock()
-
 
         self.over = False
         self.display = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
         self.background = pygame.image.load('backgroundsky.jpg')
+
+        self.clap_sound = pygame.mixer.Sound("clap.wav")
 
         self.group = pygame.sprite.LayeredUpdates()
         self.speed = 7
@@ -39,9 +39,16 @@ class Game():
 
     def checkclick(self, pos):
         sprtlist = self.group.get_sprites_at(pos)
+        
+        if sprtlist:
+            self.play_clap_sound()
+
         for k in sprtlist:
             k.kill()
             self.score = self.score + 1
+        
+    def play_clap_sound(self):
+        pygame.mixer.Sound.play(self.clap_sound)
 
     def show_score(self):
         score = self.font.render("Score : " + str(self.score), True, (255, 255, 255))
